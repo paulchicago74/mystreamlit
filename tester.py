ec30f74f6dccfe3e71064f39ddb5182e86508fca
paulchicago74 <paulchicago74@gmail.com>
2021-11-19 14:00:26 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -66,19 +66,5 @@ if(st.button('Calculate BMI')):
 
 
 
-
-bars = alt.Chart('bmi').mark_bar().encode(
-    x='wheat:Q',
-    y="year:O"
-)
-
-text = bars.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='wheat:Q'
-)
-
-(bars + text).properties(height=900)
+st.bar_chart(data=bmi, width=100, height=100, use_container_width=True)
       
