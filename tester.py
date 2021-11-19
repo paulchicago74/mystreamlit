4cc9199058a629321e8427df9d14a23bde779012
paulchicago74 <paulchicago74@gmail.com>
2021-11-19 14:03:11 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -65,9 +65,9 @@ if(st.button('Calculate BMI')):
     
 
 
->>> chart_data = pd.DataFrame(
-...     np.random.randn(50, 3),
-...     columns=["a", "b", "c"])
-...
->>> st.bar_chart(chart_data)
+chart_data = pd.DataFrame(
+np.random.randn(50, 3),
+columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
       
