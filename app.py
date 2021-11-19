ed9cae79761162995262e5f7fa879a0e31c7def1
paulchicago74 <paulchicago74@gmail.com>
2021-11-18 20:00:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,3 +69,45 @@ def main():
   df = transform(df)
   explore(df)
 main()
+
+import streamlit as st
+import pandas as pd
+def explore(df):
+  # DATA
+  st.write('Data:')
+  st.write(df)
+  # SUMMARY
+  df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
+  numerical_cols = df_types[~df_types['Data Type'].isin(['object',
+                   'bool'])].index.values
+  df_types['Count'] = df.count()
+  df_types['Unique Values'] = df.nunique()
+  df_types['Min'] = df[numerical_cols].min()
+  df_types['Max'] = df[numerical_cols].max()
+  df_types['Average'] = df[numerical_cols].mean()
+  df_types['Median'] = df[numerical_cols].median()
+  df_types['St. Dev.'] = df[numerical_cols].std()
+  st.write('Summary:')
+  st.write(df_types)
+def get_df(file):
+  # get extension and read file
+  extension = file.name.split('.')[1]
+  if extension.upper() == 'CSV':
+    df = pd.read_csv(file)
+  elif extension.upper() == 'XLSX':
+    df = pd.read_excel(file, engine='openpyxl')
+  elif extension.upper() == 'PICKLE':
+    df = pd.read_pickle(file)
+  return df
+def main():
+  st.title('Explore a dataset')
+  st.write('A general purpose data exploration app')
+file = st.file_uploader("Upload file", type=['csv' 
+                                             ,'xlsx'
+                                             ,'pickle'])
+  if not file:
+    st.write("Upload a .csv or .xlsx file to get started")
+    return
+  df = get_df(file)
+  explore()
+main()
