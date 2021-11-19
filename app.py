3e8e676b6c3fb6f584c4393d1b0cd82547743b9e
paulchicago74 <paulchicago74@gmail.com>
2021-11-18 18:32:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,5 @@
 import streamlit as st
 st.title('Explore a dataset')
 st.write('A general purpose data exploration app')
-
-import streamlit as st
-st.title('Explore a dataset')
-st.write('A general purpose data exploration app')
 file = st.file_uploader("Upload file", type=['csv'])
 st.write(file)
