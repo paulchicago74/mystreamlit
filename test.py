5c2b61e71e65d50d2b41dd3081554f9fd38da744
paulchicago74 <paulchicago74@gmail.com>
2021-11-20 17:02:30 -0600

    Update test.py

--- test.py
+++ test.py
@@ -32,16 +32,7 @@ st.metric('D value', D, delta=None, delta_color="normal")
 
 
   
-    with st.echo():
-        from st_card import st_card
-        cols = st.columns(3)
-        with cols[0]:
-            st_card('Orders', value=1200, delta=-45, delta_description='since last month')
-        with cols[1]:
-            st_card('Competed Orders', value=76.4, unit='%', show_progress=True)
-        with cols[2]:
-            st_card('Profit', value=45000, unit='($)', delta=48, use_percentage_delta=True, delta_description='since last year')
-          
+    
 # st.button('Calculate BMI'):
  # st.write(Temp)
   
