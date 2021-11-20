b9adde279c5c818cd6cdf72ab635e20061da524b
paulchicago74 <paulchicago74@gmail.com>
2021-11-19 21:46:20 -0600

    Update test.py

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ Temp = st.slider('Enter the temperature', 0, 130, 25)
 
 Time = st.slider('Enter the Time', 0, 130, 25)
 
-BMI = Time + (10 ** (Temp - Tref / Zref) + (10 ** Tref))
+BMI = Time + ((10 ** (Temp - Tref) / Zref) + (10 ** (Tref) / Zref))
 
 st.metric('Temp', BMI, delta=None, delta_color="normal")
 
