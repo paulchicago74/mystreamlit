4905610fdc625a8c9464eb6f1056648a42b5c30d
paulchicago74 <paulchicago74@gmail.com>
2021-11-20 17:48:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ st.metric('Temp', BMI, delta=None, delta_color="normal")
 
 st.metric('D value', D, delta=None, delta_color="normal")
 
-col1, col2, col3 = st.columns(3)
+col1 = st.columns(3)
 col1.write (D)
       
 
