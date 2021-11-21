fed7d146e4fc5814f03c8d2a58ce99d0fb7c25bb
paulchicago74 <paulchicago74@gmail.com>
2021-11-21 09:41:19 -0600

    Update test.py

--- test.py
+++ test.py
@@ -39,12 +39,15 @@ col2.write (BMI)
 col3.write (D)
 
 Pub1 = (10 ** ((Temp - 120)/10))/1 * Time / 3
+Pub2 = (10 ** ((Temp - 220)/10))/1 * Time / 3
 
 
 
 option = st.selectbox('choose',
  ('Email', 'Home phone', 'Mobile phone'))
 if option == 'Email': st.write(Pub1)
+if option == 'Home phone': st.write(Pub2)
+ 
 #st.write('You selected:', option)
           
 #else:
