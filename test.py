43260249b6840d6228223ae301c3da001fc43d63
paulchicago74 <paulchicago74@gmail.com>
2021-11-22 16:20:01 -0600

    Update test.py

--- test.py
+++ test.py
@@ -98,8 +98,38 @@ else:
 if selected_options == 'Email':  st.write ('Pub1', millify(Pub1, precision=2), delta=None, delta_color="normal")
 st.write (selected_options)
     
-selection = st.multiselect('Select',['BMI'])
-st.write(selection)
+options = ("male", "female")
+i1 = st.multiselect("multiselect 1", options)
+st.text("value 1: %s" % i1)
+
+i2 = st.multiselect("multiselect 2", options, format_func=lambda x: x.capitalize())
+st.text("value 2: %s" % i2)
+
+i3 = st.multiselect("multiselect 3", [])
+st.text("value 3: %s" % i3)
+
+i4 = st.multiselect("multiselect 4", ["coffee", "tea", "water"], ["tea", "water"])
+st.text("value 4: %s" % i4)
+
+i5 = st.multiselect(
+    "multiselect 5",
+    list(
+        map(
+            lambda x: f"{x} I am a ridiculously long string to have in a multiselect, so perhaps I should just not wrap and go to the next line.",
+            range(5),
+        )
+    ),
+)
+st.text("value 5: %s" % i5)
+
+if st._is_running_with_streamlit:
+
+    def on_change():
+        st.session_state.multiselect_changed = True
+
+    st.multiselect("multiselect 6", options, key="multiselect6", on_change=on_change)
+    st.text("value 6: %s" % st.session_state.multiselect6)
+    st.text(f"multiselect changed: {'multiselect_changed' in st.session_state}")
 
 
 
