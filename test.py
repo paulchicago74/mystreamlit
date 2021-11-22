4f2a2030be952a911fe0e13c611e5b8f37fb4e52
paulchicago74 <paulchicago74@gmail.com>
2021-11-22 13:26:16 -0600

    Update test.py

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ submit = form.form_submit_button('Submit')
 st.write('Press submit to have your name printed below')
 
 if submit:
-    st.write.success (BMI)
+    st.success (BMI)
   
 #chart_data = Pub1
 #st.bar_chart(chart_data)
