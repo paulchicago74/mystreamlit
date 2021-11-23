0bb632fd808cd8f5ad56184bc420d2f78797b0a1
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 12:12:54 -0600

    Update test.py

--- test.py
+++ test.py
@@ -112,10 +112,7 @@ st.text("value 1: %s" % i1)
 print(BMI)
 
 
-from link_button import link_button
-        _clicked = link_button(name = 'Click Me!', url = 'https://docs.streamlit.io/en/stable/')
-        if _clicked:    
-            st.balloons()
+st.button('Say hello', on_click="http://google.com)
 
 
 
