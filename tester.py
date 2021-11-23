42a09ea1d41374c96517938cfa93015698a38ee9
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 14:09:30 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -7,6 +7,16 @@ import pandas as pd
 import numpy as np
 # =(Time+(10^((Temp-Tref)/Zref)+(10^((Tref-Tref)/Zref)))/2*(Time)) 
 # give a title to our app
+
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
+  
+# Create the pandas DataFrame
+df = pd.DataFrame(data, columns = ['Name', 'Age'])
+  
+# print dataframe.
+df
+
+
 df1 = pd.DataFrame(
 np(50, 20),
 columns=('col %d' % i for i in range(20)))
