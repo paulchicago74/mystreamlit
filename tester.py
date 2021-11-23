d2a649ce7922224d496d8d66793b1c7f5269562f
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 15:07:04 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -21,7 +21,7 @@ T2 = Time / 10
 data = [[0, 0, 0]]
 df = pd.DataFrame(data, columns = ['Time', 'Fvalue', 'Dvalue'])
 my_table = st.table(df)
-df2 = ('Time' df + T2, df + 2, df + 3)
+# df2 = ('Time' df + T2, df + 2, df + 3)
 df2 = ({'Time': [df + T2, 3, 4],
                    'Fvalue': [360, 180, 360]},
 my_table.add_rows(df2)
