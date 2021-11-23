976f3e9cbe5c5d1291a06b40c9914e4ded224040
paulchicago74 <paulchicago74@gmail.com>
2021-11-22 18:40:40 -0600

    Update test.py

--- test.py
+++ test.py
@@ -104,12 +104,5 @@ options = ("male", "female")
 i1 = st.multiselect("selectbox 1", options)
 st.text("value 1: %s" % i1)
 
-i2 = st.multiselect("selectbox 2", options, format_func=lambda x: x.capitalize())
-st.text("value 2: %s" % i2)
 
-i3 = st.multiselect("selectbox 3", [])
-st.text("value 3: %s" % i3)
-
-i4 = st.multiselect("selectbox 4", ["coffee", "tea", "water"], ["tea", "water"])
-st.text("value 4: %s" % i4)
     
