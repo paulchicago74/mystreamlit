d385d6d4187546223e453ae42c9451e74ca91c51
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 13:58:43 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -9,13 +9,13 @@ import numpy as np
 # =(Time+(10^((Temp-Tref)/Zref)+(10^((Tref-Tref)/Zref)))/2*(Time)) 
 # give a title to our app
 df1 = pd.DataFrame(
-...    np.random.randn(50, 20),
-...    columns=('col %d' % i for i in range(20)))
-...
->>> my_table = st.table(df1)
->>>
->>> df2 = pd.DataFrame(
-...    np.random.randn(50, 20),
-...    columns=('col %d' % i for i in range(20)))
-...
->>> my_table.add_rows(df2)
+np.random.randn(50, 20),
+columns=('col %d' % i for i in range(20)))
+
+my_table = st.table(df1)
+
+df2 = pd.DataFrame(
+np.random.randn(50, 20),
+columns=('col %d' % i for i in range(20)))
+
+my_table.add_rows(df2)
