56f1f48a5f79e7a677b484fee1574105269716d6
paulchicago74 <paulchicago74@gmail.com>
2021-11-22 18:41:20 -0600

    Update test.py

--- test.py
+++ test.py
@@ -100,7 +100,7 @@ else:
 if selected_options == 'Email':  st.success ('Pub1', millify(Pub1, precision=2), delta=None, delta_color="normal")
 #st.write (selected_options)
 
-options = ("male", "female")
+options = ("male": BMI, "female")
 i1 = st.multiselect("selectbox 1", options)
 st.text("value 1: %s" % i1)
 
