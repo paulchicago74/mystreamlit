f09e7fb2e43b92d1c636cff5c2d06d9244cfd8e8
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 14:00:42 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -8,7 +8,7 @@ import numpy as np
 # =(Time+(10^((Temp-Tref)/Zref)+(10^((Tref-Tref)/Zref)))/2*(Time)) 
 # give a title to our app
 df1 = pd.DataFrame(
-np.random.randn(50, 20),
+np(50, 20),
 columns=('col %d' % i for i in range(20)))
 
 my_table = st.table(df1)
