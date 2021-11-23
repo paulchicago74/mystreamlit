05a3511b4c3c4aa3cdaa1860fac7f5ecdf3d2a74
paulchicago74 <paulchicago74@gmail.com>
2021-11-23 13:39:26 -0600

    Update test.py

--- test.py
+++ test.py
@@ -112,7 +112,7 @@ i1 = st.multiselect("selectbox 1", options)
 st.text("value 1: %s" % i1)
 print(BMI)
 
-_clicked = link_button(name = 'Click Me!', url = 'https://docs.streamlit.io/en/stable/')
+#_clicked = link_button(name = 'Click Me!', url = 'https://docs.streamlit.io/en/stable/')
 
 
 
