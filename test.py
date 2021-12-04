af13c278e085c1351a6f980e9b244ea14d13eae4
paulchicago74 <paulchicago74@gmail.com>
2021-12-04 14:55:35 -0600

    Update test.py

--- test.py
+++ test.py
@@ -57,6 +57,10 @@ Dvalue8 = Fvalue8 / Dvalue
 Dvalue9 = Fvalue9 / Dvalue
 Dvalue10 = Fvalue10 / Dvalue
 
+df = pd.DataFrame({
+    'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
+    #'D-value': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
+})
 
 #Tref = 120
 
