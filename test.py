f825599a43e3c6be183220c220ebbfaf1249bfec
paulchicago74 <paulchicago74@gmail.com>
2021-12-04 15:37:54 -0600

    Update test.py

--- test.py
+++ test.py
@@ -140,8 +140,7 @@ st.text("value 1: %s" % i1)
 print(BMI)
 
 #_clicked = link_button(name = 'Click Me!', url = 'https://docs.streamlit.io/en/stable/')
-chart = st.line_chart(df)
-chart = st.line_chart(df2)
+
 
 col1, col2 = st.columns(2)
 chart = col1.line_chart(df)
