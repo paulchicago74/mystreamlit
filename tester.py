bc0bda9645d4918935998856a6a8c4eeda1a3cb2
paulchicago74 <paulchicago74@gmail.com>
2021-12-05 09:49:41 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,30 +1,39 @@
-import pdfkit
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
-from datetime import date
+# import the streamlit library
 import streamlit as st
-from streamlit.components.v1 import iframe
-import pandas as pd
-import numpy as np
-import streamlit_gchart as gchart
-import plotly.offline as offline
-import plotly.graph_objs as go
 import altair as alt
-import plotly.figure_factory as ff
-import plotly.express as px
-from sklearn.linear_model import LinearRegression
-from statsmodels.formula.api import ols
-from sklearn.metrics import mean_squared_error, r2_score
-import statsmodels.api as sm
+import pandas as pd
 from millify import millify
+from pandas_profiling import ProfileReport
+import numpy as np
+#from link_button import link_button
+# =(Time+(10^((Temp-Tref)/Zref)+(10^((Tref-Tref)/Zref)))/2*(Time)) 
+# (10^((Temp-Tref)/Zref))/1*Time
+# give a title to our app
+st.sidebar.title('Predictive Model Calculation')
+ 
+# TAKE WEIGHT INPUT in kgs
+# Tref = st.number_input('Enter the reference temperature', min_value=20)
 
-Temp = 150
-Tref = 144
-Zref = 10
-Time = 25
+Zref = st.sidebar.number_input('Enter the reference Zvalue', value=5.0, min_value=1.0)
 
-Wanted_D = st.sidebar.number_input('Enter the Dvalue', min_value=0.1, step=0.1)
-Dvalue = st.sidebar.number_input('Enter the reference Dvalue', min_value=0.1, step=0.1)
+Dvalue = st.sidebar.number_input('Enter the reference Dvalue', value=5.0, min_value=0.1, step=0.1)
 
+Tref = st.sidebar.number_input('Enter the reference Tref', value=150.0, min_value=0.1, step=0.1)
+
+st.sidebar.subheader('Parameters', anchor=None)
+
+Temp = st.sidebar.slider('Enter the temperature', min_value=1.00, max_value=250.00, value=120.00, step=0.5)
+
+Time = st.sidebar.slider('Enter the Time', value=10.00, min_value=0.1, max_value=200.0, step=0.5)
+
+genre = st.sidebar.radio(
+    "Predict minumum D-value required?",
+    ('Yes', 'No'))
+if genre == 'Yes': Wanted_D = st.sidebar.number_input('Enter the Dvalue', min_value=0.1, step=0.1)
+else:
+    st.write("")
+
+#Wanted_D = st.sidebar.number_input('Enter the Dvalue', min_value=0.1, step=0.1)
 Fvalue0 = 0
 Fvalue1 = (Fvalue0 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
 Fvalue2 = (Fvalue1 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/9) - (Time/10)))
@@ -49,17 +58,10 @@ Dvalue8 = Fvalue8 / Dvalue
 Dvalue9 = Fvalue9 / Dvalue
 Dvalue10 = Fvalue10 / Dvalue
 
-#D= Fvalue / Dvalue
-
-metric = 100 / 10
-
-# sample data
 df = pd.DataFrame({
     'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
     #'D-value': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
 })
-
-#df
 
 df2 = pd.DataFrame({
     #'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
@@ -67,11 +69,6 @@ df2 = pd.DataFrame({
     #'Time': [0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1],
     
 })
-
-df2
-
-chart = st.line_chart(df)
-chart = st.line_chart(df2)
 
 y = np.array([Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]).reshape((-1, 1))
 #x = np.array([0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1]).reshape((-1, 1))
@@ -90,9 +87,86 @@ b1 = Sxy/Sxx
 b0 = y_mean-b1*x_mean
 #st.write('slope b1 is', b1)
 #st.write('intercept b0 is', b0)
-st.write('The time you need for a D-value of', Wanted_D ,'is' , millify(b1*Wanted_D + b0, precision=2))
+if genre == 'Yes': st.write('The time you need for a D-value of', Wanted_D ,'is' , millify(b1*Wanted_D + b0, precision=2))
 
 
 
+#Tref = 120
 
-           
+#BMI = (10 ** ((Temp - Tref)/Zref))/1 * Time
+
+#D= BMI / Dvalue
+
+#st.metric('Temp', millify(BMI, precision=2), delta=None, delta_color="normal")
+
+#st.metric('D value', millify(D, precision=2), delta=None, delta_color="normal")
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric('F-value', millify(Fvalue10, precision=2), delta=None, delta_color="normal")
+
+col2.metric('D value', millify(Dvalue10, precision=2), delta=None, delta_color="normal")
+
+if genre == 'Yes': col3.metric('Predicted time for the D-value', millify(b1*Wanted_D + b0, precision=2), delta=None, delta_color="normal")
+else:
+    st.write("")
+
+#st.success (BMI)
+
+Pub1 = (10 ** ((Temp - Tref)/10))/1 * Time / 3
+#Tref = st.empty()
+#Tref = 200
+Pub2 = (10 ** ((Temp - Tref)/10))/1 * Time / 3
+#Tref = st.empty()
+#Tref = 180
+#Pub1.replace (BMI(Tref, 200))
+#Pub1 = BMI.replace(Tref, 200)
+
+#option = st.multiselect('choose',
+# ('Email', 'Home phone', 'Mobile phone'))
+#if option == 'Email': st.write(Pub1) 
+#if option == 'Email': Tref = 1200
+#if option == 'Email':  st.metric(Pub1, millify(Pub1, precision=2), delta=None, delta_color="normal")
+#st.write (option)
+#if option == 'Home phone': st.write(BMI)
+
+form = st.form(key='my-form')
+name = form.text_input('Enter your name')
+submit = form.form_submit_button('Submit')
+
+st.write('Press submit to have your name printed below')
+
+if submit:
+    st.success (BMI)
+  
+#chart_data = Pub1
+#st.bar_chart(chart_data)
+
+#container = st.container()
+#all = st.checkbox("Select all")
+ 
+#if all:
+#    selected_options = container.multiselect("Select one or more options:",
+#         ["Email", 'Home phone', 'Mobile phone'],['Email', 'Home phone', 'Mobile phone'])
+#else:
+#    selected_options =  container.multiselect("Select one or more options:",
+#        ['Email', 'Home phone', 'Mobile phone'])
+  
+#if selected_options == 'Email':  st.success ('Pub1', millify(Pub1, precision=2), delta=None, delta_color="normal")
+#st.write (selected_options)
+
+#options = ("male", "female")
+#i1 = st.multiselect("selectbox 1", options)
+#st.text("value 1: %s" % i1)
+#print(BMI)
+
+#_clicked = link_button(name = 'Click Me!', url = 'https://docs.streamlit.io/en/stable/')
+
+
+col1, col2 = st.columns(2)
+chart = col1.line_chart(df)
+chart = col2.line_chart(df2)
+
+
+
+    
