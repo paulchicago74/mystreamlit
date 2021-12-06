487c78d81ee39c1e627b5bca049ca29da1e487ca
paulchicago74 <paulchicago74@gmail.com>
2021-12-05 19:15:40 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -76,7 +76,7 @@ Dvalue8 = Fvalue8 / Dvalue
 Dvalue9 = Fvalue9 / Dvalue
 Dvalue10 = Fvalue10 / Dvalue
 
-df3['sums'] = df3.sum(axis=1)
+df3['sums'] = (Fvalue0 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
 st.write(df3)
 
 df = pd.DataFrame({
