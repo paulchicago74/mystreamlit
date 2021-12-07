8fe1d7b5c6ab0d1f907be6fade2c62276bb16cc0
paulchicago74 <paulchicago74@gmail.com>
2021-12-07 17:23:48 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -76,8 +76,9 @@ Dvalue8 = Fvalue8 / Dvalue
 Dvalue9 = Fvalue9 / Dvalue
 Dvalue10 = Fvalue10 / Dvalue
 
-df3['sums'] = df3.(Fvalue0 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
-st.write(df3)
+if uploaded_file is not None:
+    df3['sums'] = (Fvalue0 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
+    st.write(df3)
 
 df = pd.DataFrame({
     'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
