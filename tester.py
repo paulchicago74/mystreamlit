f6080fd77e3397a3e727a8138db4cce361f86f41
paulchicago74 <paulchicago74@gmail.com>
2021-12-08 17:54:56 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -85,7 +85,7 @@ if uploaded_file is not None:
     df3["Fo"] = ((10 ** ((df3["Temp"] - Tref)/Zref) + (10 ** ((df3["Temp"] - Tref)/Zref)))/2*((df3["Time"])-(df3["Time"].shift())))
     st.write(df3)
     
-     df3["F1"] = ((df3["Fo"].shift()) + (10 ** ((df3["Temp"] - Tref)/Zref) + (10 ** ((df3["Temp"] - Tref)/Zref)))/2*((df3["Time"])-(df3["Time"].shift())))
+    df3["F1"] = ((df3["Fo"].shift()) + (10 ** ((df3["Temp"] - Tref)/Zref) + (10 ** ((df3["Temp"] - Tref)/Zref)))/2*((df3["Time"])-(df3["Time"].shift())))
     st.write(df3)
     
     
