2e5ac17b0ae554a136f2e7a6f80323b1eef76341
paulchicago74 <paulchicago74@gmail.com>
2021-12-08 19:06:33 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -141,6 +141,10 @@ if genre == 'Yes': st.write('The time you need for a D-value of', Wanted_D ,'is'
 
 col1, col2, col3 = st.columns(3)
 
+if uploaded_file is not None:
+    col1.metric('F-value', millify(sum(df3["Fo"]), precision=2), delta=None, delta_color="normal")
+    
+
 col1.metric('F-value', millify(Fvalue10, precision=2), delta=None, delta_color="normal")
 
 col2.metric('D value', millify(Dvalue10, precision=2), delta=None, delta_color="normal")
