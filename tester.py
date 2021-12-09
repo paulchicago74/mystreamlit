04e101ebc71c100b4f7f52ea191ca659ffc1289f
paulchicago74 <paulchicago74@gmail.com>
2021-12-08 19:30:24 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -147,9 +147,7 @@ if uploaded_file is not None:
 else:
     col1.metric('F-value', millify(Fvalue10, precision=2), delta=None, delta_color="normal")
     col2.metric('D value', millify(Dvalue10, precision=2), delta=None, delta_color="normal")
-
-
-
+    col3.metric('Predicted time for the D-value', millify(b1*Wanted_D + b0, precision=2), delta=None, delta_color="normal")
 
 if genre == 'Yes': col3.metric('Predicted time for the D-value', millify(b1*Wanted_D + b0, precision=2), delta=None, delta_color="normal")
 else:
