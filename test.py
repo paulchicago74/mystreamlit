1cdfd4ba2ef2e03b17109b7965abcab79e91683f
paulchicago74 <paulchicago74@gmail.com>
2021-12-27 13:21:54 -0600

    Update test.py

--- test.py
+++ test.py
@@ -184,7 +184,7 @@ pdf.output('test.pdf', 'F')
 
 st.download_button(
      label="Download data as CSV",
-     data=pdf,
-     file_name='test.pdf',
-     mime='text/pdf',
+     data=file,
+     file_name="barchart.png",
+     mime="image/png",
  )
