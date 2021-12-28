b7f5195afb0cbfcc9556be2f194011c47d2075ea
paulchicago74 <paulchicago74@gmail.com>
2021-12-28 15:07:20 -0600

    Update test.py

--- test.py
+++ test.py
@@ -72,7 +72,7 @@ df = pd.DataFrame({
 df2 = pd.DataFrame({
     #'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
     'Dvalue': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10],
-    #'Time': [0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1],
+    'Time': [0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1],
     
 })
 
