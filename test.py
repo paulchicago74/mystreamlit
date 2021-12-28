ab5d2b46bd71f0608d6089c4e42e768fb08c5582
paulchicago74 <paulchicago74@gmail.com>
2021-12-28 15:08:14 -0600

    Update test.py

--- test.py
+++ test.py
@@ -184,7 +184,7 @@ st.download_button(
  )
 
 
-st.altair_chart(alt.Chart(pd.DataFrame(df), height=500, width=500)
+st.altair_chart(alt.Chart(pd.DataFrame(df2), height=500, width=500)
                .mark_line(color='#0068c9', opacity=0.5)
                .encode(
                x='Dvalue',
