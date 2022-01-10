5f8cce7c587348439850617698b7e55eba6b54e7
paulchicago74 <paulchicago74@gmail.com>
2022-01-09 18:42:56 -0600

    Update test.py

--- test.py
+++ test.py
@@ -208,32 +208,6 @@ chart = col2.altair_chart(alt.Chart(pd.DataFrame(df))
 
 
 
-def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-
-df = load_iris(as_frame=True)["data"]
-
-
-figs = []
-
-for col in df.columns:
-    fig, ax = plt.subplots()
-    ax.plot(df[col])
-    st.pyplot(fig)
-    figs.append(fig)
-
-export_as_pdf = st.button("Export Report")
-
-if export_as_pdf:
-    pdf = FPDF()
-    for chart:
-        pdf.add_page()
-        with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
-                fig.savefig(tmpfile.name)
-                pdf.image(tmpfile.name, 10, 10, 200, 100)
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "testfile")
-    st.markdown(html, unsafe_allow_html=True)
 
 
