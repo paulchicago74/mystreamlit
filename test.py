543d8dd2f5da6f85bc72b330594c1306272ffbc9
paulchicago74 <paulchicago74@gmail.com>
2022-01-12 18:17:06 -0600

    Update test.py

--- test.py
+++ test.py
@@ -205,7 +205,19 @@ chart = col2.altair_chart(alt.Chart(pd.DataFrame(df))
 
 #chart.save('chart.html', embed_options={'renderer':'svg'})
 
+pdf = FPDF()  # pdf object
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.add_page()
 
+pdf.set_font("Times", "B", 18)
+pdf.set_xy(10.0, 20)
+pdf.cell(w=75.0, h=5.0, align="L", txt="This is my sample text")
+
+st.download_button(
+    "Download Report",
+    data=pdf.output(dest='S').encode('latin-1'),
+    file_name="Output.pdf",
+)
 
 
 
