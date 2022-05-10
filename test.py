43afd3c9921cd1887d37634e14ff08307738a964
paulchicago74 <paulchicago74@gmail.com>
2022-05-10 14:32:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -238,6 +238,7 @@ for i in data2:
 for i in Tref:
     pdf.write(5, "The time you need for a D-value of ") 
     pdf.write(5, str(i))
+    pdf.write(5, "The time you need for a D-value of ") 
     pdf.ln()
 pdf.write(5, "Zref")    
 pdf.output("testings.pdf")
