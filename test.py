3544480e51506760b1bf84bec686b5ac26a3c0d6
paulchicago74 <paulchicago74@gmail.com>
2022-05-11 16:37:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -235,7 +235,7 @@ pdf.add_page()
 pdf.set_font("Arial", size=12)
 pdf.image("logo.png", x=5, y=5, w=60, h=30)
 pdf.ln()
-
+pdf.write(5, "Zref", align="C")
 for i in data2:
     #pdf.image("logo.png", x=5, y=5, w=60, h=30)
     pdf.write(55, "Dvalue", "Zref = ") 
