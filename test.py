0a08672b93da23d722642f04fd5400d070d54b82
paulchicago74 <paulchicago74@gmail.com>
2022-05-11 16:21:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -239,7 +239,7 @@ pdf.ln()
 for i in data2:
     #pdf.image("logo.png", x=5, y=5, w=60, h=30)
     pdf.write(45, "Dvalue", "Zref = ") 
-    pdf.write(5, str(i))
+    pdf.write(45, str(i))
     pdf.ln()
     
 for i in Tref:
