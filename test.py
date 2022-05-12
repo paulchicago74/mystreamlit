84906f92f166d034c79cb43d2bac932837a365e5
paulchicago74 <paulchicago74@gmail.com>
2022-05-11 19:12:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -208,8 +208,8 @@ chart = col2.altair_chart(alt.Chart(pd.DataFrame(df))
                
                ).interactive())
 
-chart.save('chart.html', embed_options={'renderer':'svg'})
-
+#chart.save('chart.html', embed_options={'renderer':'svg'})
+chart.save('chart.png')
 data = st.write(Dvalue10)
 
 pdf = FPDF()  # pdf object
