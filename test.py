180692d0b07be63cb4317256de16ddef9d6e616f
paulchicago74 <paulchicago74@gmail.com>
2022-05-12 13:25:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -200,7 +200,7 @@ chart = col1.altair_chart(alt.Chart(pd.DataFrame(df2))
                tooltip=['Time', 'Dvalue'],
                
                ).interactive())
-chart = col2.altair_chart(alt.Chart(pd.DataFrame(df))
+chart = col2.altair_chart(alt.Chart(pd.DataFrame(df), title="Evolution of stock prices")
                .mark_line(color='#0068c9', opacity=0.5, point=alt.OverlayMarkDef(color="pink"))
                .encode(
                y='F-value',
