382e52ba9ec3c2c65e8a4a769e8708be879af4a9
paulchicago74 <paulchicago74@gmail.com>
2022-05-29 08:31:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -265,16 +265,7 @@ def header(self):
         self.ln(20)
         
 
-def footer(self):
-        # Go to 1.5 cm from bottom
-        self.cell(-15)
-        # Select Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Print centered page number
-        self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
-       
-
-pdf.cell(20, 10, 'Title', 1, 1, 'C')      
+#pdf.cell(20, 10, 'Title', 1, 1, 'C')      
 pdf.image("logo.png", x=5, y=5, w=60, h=30)
 pdf.ln()
 
