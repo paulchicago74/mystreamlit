74b0b26d4cdf0370faffed791df4564e3a320b10
paulchicago74 <paulchicago74@gmail.com>
2022-05-30 11:21:39 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -54,7 +54,7 @@ reference = st.sidebar.radio(
     ('Yes', 'No'))
 
 if reference == 'Yes': option = st.selectbox('How would you like to be contacted?', ('Paper1', 'Paper2', 'Paper3')) 
-    #if option == 'Paper1' : Dvalue = 5 
+    if option == 'Paper1' : Dvalue = 5 
     #if option == 'Paper1' : Zref = 10 
     #if option == 'Paper1' : Tref = 150
     #if option == 'Paper2' : Dvalue = 10
