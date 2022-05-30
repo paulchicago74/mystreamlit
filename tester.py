58b97a99c62aa660e02645c00a56ef5486cde2a2
paulchicago74 <paulchicago74@gmail.com>
2022-05-30 10:06:31 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -57,11 +57,11 @@ if reference == 'Yes': option = st.selectbox('How would you like to be contacted
 
 else:
     st.write("")
-#if option == 'Paper1' : Dvalue = 5 
-#if option == 'Paper1' : Zref = 10 
-#if option == 'Paper1' : Tref = 150
-#if option == 'Paper2' : Dvalue = 10
-#if option == 'Paper3' : Dvalue = 15
+if option == 'Paper1' : Dvalue = 5 
+if option == 'Paper1' : Zref = 10 
+if option == 'Paper1' : Tref = 150
+if option == 'Paper2' : Dvalue = 10
+if option == 'Paper3' : Dvalue = 15
 
 #Tref = st.sidebar.number_input('Enter the reference Tref', value=150.0, min_value=0.1, step=0.1)
 
