929df72ae9485a9e187c95f33ff8ecf6fff75fbf
paulchicago74 <paulchicago74@gmail.com>
2022-05-30 11:40:02 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -43,7 +43,7 @@ if uploaded_file is not None:
 # TAKE WEIGHT INPUT in kgs
 # Tref = st.number_input('Enter the reference temperature', min_value=20)
    
-#Zref = st.sidebar.number_input('Enter the reference Zvalue', value=5.0, min_value=1.0)
+Zref = st.sidebar.number_input('Enter the reference Zvalue', value=5.0, min_value=1.0)
 
 Tref = st.sidebar.number_input('Enter the reference Tref', value=150.0, min_value=0.1, step=0.1)
 
@@ -53,8 +53,7 @@ reference = st.sidebar.radio(
     "Using a scientific reference?",
     ('Yes', 'No'), key='my-form')
 
-if reference == 'Yes': 
-option = st.selectbox('How would you like to be contacted?', ('Paper1', 'Paper2', 'Paper3')) 
+if reference == 'Yes': option = st.selectbox('How would you like to be contacted?', ('Paper1', 'Paper2', 'Paper3')) 
 #if reference == 'Yes': Zref = st.sidebar.number_input('Enter the reference Zvalue', value=5.0, min_value=1.0)
 if option == 'Paper1' : Dvalue = 5 
     #if option == 'Paper1' : Zref = 10 
