2f8a1b7446118fa9036da93288ef8189dd34bb6e
paulchicago74 <paulchicago74@gmail.com>
2022-05-30 11:52:09 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -53,7 +53,7 @@ reference = st.sidebar.radio(
     "Using a scientific reference?",
     ('Yes', 'No'), key='my-form')
 
-if reference == 'Yes': option = st.selectbox('How would you like to be contacted?', ('Paper1', 'Paper2', 'Paper3')) 
+if reference == 'No': st.write("") 
 #if reference == 'Yes': Zref = st.sidebar.number_input('Enter the reference Zvalue', value=5.0, min_value=1.0)
     #if option == 'Paper1' : Dvalue = 5 
     #if option == 'Paper1' : Zref = 10 
@@ -62,7 +62,7 @@ if reference == 'Yes': option = st.selectbox('How would you like to be contacted
     #if option == 'Paper3' : Dvalue = 15
     
 else:
-    st.write("")
+    option = st.selectbox('How would you like to be contacted?', ('Paper1', 'Paper2', 'Paper3')) 
     
     
 if option == 'Paper1' : Dvalue = 5
