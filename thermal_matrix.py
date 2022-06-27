cc50b7e96d2288cc49c6cb1d4eedd9ede7f5405f
paulchicago74 <paulchicago74@gmail.com>
2022-06-27 11:34:13 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+st.set_page_config(layout='wide')
+st.sidebar.title('Thermal Matrix Calculation')
 
-st.selectbox(
+st.sidebar.selectbox(
      'How would you like to be contacted?',
      ('Email', 'Home phone', 'Mobile phone'))
