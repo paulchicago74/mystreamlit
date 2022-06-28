812ea85e8047119f6676ec10f1c3df20d73d1204
paulchicago74 <paulchicago74@gmail.com>
2022-06-28 10:21:48 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -27,7 +27,7 @@ st.write('Values:', values3)
 
 number = st.sidebar.number_input('Fahrenheit to Celsius conversion')
 celsius = (number-32)*5/9
-if number > 0: st.sidebar.text('Temp in C is', celsius)
+if number > 0: st.sidebar.subheader('Temp in C is', celsius)
 
 
 J = 5 * (10**((80 - values2)/5.75))
