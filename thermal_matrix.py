b5c609f5adf651a433a4030f68f6ce77ad4cedf4
paulchicago74 <paulchicago74@gmail.com>
2022-07-17 15:21:03 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -66,7 +66,7 @@ if product == 'Intermediate foods' : st.metric(label = "Treatment Time", value=R
 if product == 'Acid or acidified' and ph < 4.00 : st.write(J)
 #G22*(10^((H22-L22)/I22))
 
-data= ('Temp' str(J))
+data= ('Temp', str(J))
 
 #text_contents = str(J)
 st.download_button('Download some text', data)
