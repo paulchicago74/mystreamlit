46dd5b24bb33f8f121b14ababcb26aa57d1adbf0
paulchicago74 <paulchicago74@gmail.com>
2022-07-17 14:02:43 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -69,6 +69,7 @@ if product == 'Acid or acidified' and ph < 4.00 : st.write(J)
 st.metric(label="Gas price", value=4, delta=-0.5,
      delta_color="inverse")
 
+st.download_button(label='Download' , file=f, file_name='sequence.fasta', mime= 'application/octet-stream' )
 with open("output.txt", "a") as f:
     print("Hello StackOverflow!", file=f)
     print("I have a question.", file=f)
