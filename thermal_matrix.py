4427a0f914cd0b47f1c05fba2c97b93dbe215bdd
paulchicago74 <paulchicago74@gmail.com>
2022-07-17 15:33:21 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -65,7 +65,7 @@ if product == 'Juice' and J > values3: st.error('This is an error message!')
 if product == 'Intermediate foods' : st.metric(label = "Treatment Time", value=R)
 if product == 'Acid or acidified' and ph < 4.00 : st.write(J)
      
-Title = st.markdown(*Title *)
+Title = st.markdown('*Title *')
 #G22*(10^((H22-L22)/I22))
 texto = '''Temp = '''
 data= str(J)
