6fd7ba8ad3700995ab85d0028dc5e43a99a3e63c
paulchicago74 <paulchicago74@gmail.com>
2022-07-19 09:56:38 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -3,7 +3,7 @@ from simple_colors import *
 from pytesseract import image_to_string 
 from PIL import Image
 
-text =  image_to_string(Image.open('sample.jpg'))
+text =  image_to_string(Image.open('shot.png'))
 
 
 st.set_page_config(layout='wide')
