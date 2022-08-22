d396b45a264fa2ec1ab6114ceb8c43e96c7b545c
paulchicago74 <paulchicago74@gmail.com>
2022-08-21 20:36:24 -0500

    Update process_capability.py

--- process_capability.py
+++ process_capability.py
@@ -15,6 +15,7 @@ st.sidebar.title('Process Capability Calculation')
 uploaded_file = st.sidebar.file_uploader("Upload Files",type=['csv'])
 if uploaded_file is None:
     st.write("")
+    data = 0
 
 if uploaded_file is not None:
     file_details = uploaded_file.getvalue()
