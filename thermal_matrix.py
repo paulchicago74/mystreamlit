866b258861c89b3c23952f93ed7e865d271f62aa
paulchicago74 <paulchicago74@gmail.com>
2022-08-30 11:30:37 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -18,32 +18,32 @@ st.sidebar.title('Thermal Matrix Calculation')
 
 
 def check_password():e
-	"""Returns `True` if the user had the correct password."""
+"""Returns `True` if the user had the correct password."""
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
+def password_entered():
+"""Checks whether a password entered by the user is correct."""
+if st.session_state["password"] == st.secrets["password"]:
+    st.session_state["password_correct"] = True
+    del st.session_state["password"]  # don't store password
+else:
+    st.session_state["password_correct"] = False
 
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.sidebar.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
+if "password_correct" not in st.session_state:
+# First run, show input for password.
+st.sidebar.text_input(
+    "Password", type="password", on_change=password_entered, key="password"
+)
+return False
+elif not st.session_state["password_correct"]:
+# Password not correct, show input + error.
+st.text_input(
+    "Password", type="password", on_change=password_entered, key="password"
+)
+st.error("😕 Password incorrect")
+return False
+else:
+# Password correct.
+return True
 
 if check_password():
    
