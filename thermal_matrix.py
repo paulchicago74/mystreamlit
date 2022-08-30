310514d36dc8178b7cd9aef9a04ec833019b0870
paulchicago74 <paulchicago74@gmail.com>
2022-08-30 11:14:52 -0500

    Update thermal_matrix.py

--- thermal_matrix.py
+++ thermal_matrix.py
@@ -85,45 +85,112 @@ if check_password():
 
         with col1:
             title = st.text_input('Project')
-            title2 = st.write('The current movie title is', title)
-
+            
         with col2:
             d = str(st.date_input(
             "When's your birthday"))
        
-
-
         number = st.sidebar.number_input('Fahrenheit to Celsius conversion')
         celsius = (number-32)*5/9
         if number > 0: st.sidebar.write('Temp in C:')
         if number > 0: st.sidebar.subheader(celsius)
+        round (celsius, 2)
 
-        J = 5 * (10**((80 - values2)/5.75))
-        R = 10 * (10**((90 - values2)/9))
-        ph1 = 0.1 * (10**((93.3 - values2)/8.9))
-        ph2 = 1 * (10**((93.3 - values2)/8.9))
-        ph3 = 2.5 * (10**((93.3 - values2)/8.9))
-        ph4 = 5 * (10**((93.3 - values2)/8.9))
-        ph5 = 10 * (10**((93.3 - values2)/8.9))
-        ph6 = 20 * (10**((93.3 - values2)/8.9))
+       J = 8.5 * (10**((60 - values2)/4.9))
+R = 10 * (10**((90 - values2)/9))
+#IF = 1 * (10**((72 - hftemp)/7.5))
+ph1 = 0.1 * (10**((93.3 - values2)/8.9))
+ph2 = 1 * (10**((93.3 - values2)/8.9))
+ph3 = 2.5 * (10**((93.3 - values2)/8.9))
+ph4 = 5 * (10**((93.3 - values2)/8.9))
+ph5 = 10 * (10**((93.3 - values2)/8.9))
+ph6 = 20 * (10**((93.3 - values2)/8.9))
+Cs1 = 10 * (10**((90 - values2)/10))
+Cs2 = 37 * (10**((86 - values2)/7))
 
-        if product == 'Acid or acidified' and ph < 3.9 : (st.metric(label = "Treatment Time", value=ph1, delta=values3-ph1))
-        if product == 'Acid or acidified' and 3.9 <= ph < 4.1 : (st.metric(label = "Treatment Time", value=ph2, delta=values3-ph2))
-        if product == 'Acid or acidified' and 4.1 <= ph < 4.2 : (st.metric(label = "Treatment Time", value=ph3, delta=values3-ph3))
-        if product == 'Acid or acidified' and 4.2 <= ph < 4.3 : (st.metric(label = "Treatment Time", value=ph4, delta=values3-ph4))
-        if product == 'Acid or acidified' and 4.3 <= ph < 4.4 : (st.metric(label = "Treatment Time", value=ph5, delta=values3-ph5))
-        if product == 'Acid or acidified' and 4.4 <= ph <= 4.6 : (st.metric(label = "Treatment Time", value=ph6, delta=values3-ph6))
+#ph8 = 2.5 * (10**((74.44 - hftemp)/9.5))
+
+round(ph1, 2)
+
+if product == 'Acid or acidified' and ph < 3.9 : (st.metric(label = "Treatment Time", value=round(ph1, 2), delta=round((values3-ph1),2)))
+if product == 'Acid or acidified' and 3.9 <= ph < 4.1 : (st.metric(label = "Treatment Time", value=round(ph2, 2), delta=round((values3-ph2),2)))
+if product == 'Acid or acidified' and 4.1 <= ph < 4.2 : (st.metric(label = "Treatment Time", value=round(ph3, 2), delta=round((values3-ph3),2)))
+if product == 'Acid or acidified' and 4.2 <= ph < 4.3 : (st.metric(label = "Treatment Time", value=round(ph4, 2), delta=round((values3-ph4),2)))
+if product == 'Acid or acidified' and 4.3 <= ph < 4.4 : (st.metric(label = "Treatment Time", value=round(ph5, 2), delta=round((values3-ph5),2)))
+if product == 'Acid or acidified' and 4.4 <= ph <= 4.6 : (st.metric(label = "Treatment Time", value=round(ph6, 2), delta=round((values3-ph6),2)))
+
+     
+if product == 'Juice' : (st.metric(label = "Treatment Time", value=round(J, 2), delta=values3-J))
+if product == 'Juice' and ph < 4.41 and J < values3: st.success('Process is Safe')
+if product == 'Juice' and ph < 4.41 and J > values3: st.error('Not Safe!')
+if product == 'Juice' and ph > 4.40 : st.error('Not Safe! Check for Clostridium')
+#if product == 'Juice' : hotfill == st.write("")
+#else:
+#    st.error('This is an error message!')
+
+#if product == 'Intermediate foods' : st.metric(label = "Treatment Time", value=R)
+if product == 'Intermediate foods' and ph >4.6 and values2<65 and  aw<0.80: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-88))
+if product == 'Intermediate foods' and ph >4.6 and values2<65 and  0.80 <= aw < 0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-90))
+if product == 'Intermediate foods' and ph >4.6 and values2<65 and  aw>=0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-87))
+
+if product == 'Intermediate foods' and ph >4.6 and 65 <= values2 < 72 and  aw<0.80: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-27))
+if product == 'Intermediate foods' and ph >4.6 and 65 <= values2 < 72 and  0.80 <= aw < 0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-30))
+if product == 'Intermediate foods' and ph >4.6 and 65 <= values2 < 72 and  aw>=0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-27))
+
+if product == 'Intermediate foods' and ph >4.6 and values2>=72 and  aw<0.80: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-8))
+if product == 'Intermediate foods' and ph >4.6 and values2>=72 and  0.80 <= aw < 0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-4))
+if product == 'Intermediate foods' and ph >4.6 and values2>=72 and  aw>=0.9: (st.metric(label = "Treatment Time for Salmonella", value=values3, delta=values3-2))
+
+if product == 'Intermediate foods' and ph >4.2 and aw>=0.92 and values2>=90: (st.metric(label = "Treatment Time for Clostridium spp. spores", value=round(values3,2), delta=round((values3-Cs1),2)))
+if product == 'Intermediate foods' and ph >4.2 and aw>=0.92 and values2<90: (st.metric(label = "Treatment Time for Clostridium spp. spores", value=round(values3, 2), delta=round((values3-Cs2),2)))
 
 
-        if product == 'Juice' : (st.metric(label = "Treatment Time", value=J, delta=values3-J))
-        if product == 'Juice' and J < values3: st.success('This is a success message!')
-        if product == 'Juice' and J > values3: st.error('This is an error message!')
-        #else:
-        #    st.error('This is an error message!')
+if product == 'Acid or acidified' and ph < 4.00 : st.write(J)
+#G22*(10^((H22-L22)/I22))
 
-        if product == 'Intermediate foods' : st.metric(label = "Treatment Time", value=R)
-        if product == 'Acid or acidified' and ph < 4.00 : st.write(J)
 
+	
+if hotfill == 'Yes' and product == 'Acid or acidified':	
+	st.header('Values Selected for Hot Fill')
+
+	col1, col2, col3 = st.columns(3)
+
+	with col1:
+		st.header("pH")
+		st.write('Values:', round(phhotfill, 2))
+
+	with col2:
+		st.header("Temperature")
+		st.write('Temperature:', round(hftemp, 2), "C")
+
+	with col3:
+		st.header("Time")
+		st.write('Values:', round(timehotfill,2))
+
+if hotfill == 'Yes' and product == 'Intermediate foods':	
+	st.header('Values Selected for Hot Fill')
+
+	col1, col2 = st.columns(2)
+
+	with col1:
+		st.header("Temperature")
+		st.write('Temperature:', round(hftemp, 2), "C")
+
+	with col2:
+		st.header("Time")
+		st.write('Values:', round(timehotfill,2))		
+
+
+if hotfill == 'Yes':
+	IF = 1 * (10**((72 - hftemp)/7.5))
+	if product == 'Acid or acidified' and phhotfill <= 4.1 : (st.metric(label = "Treatment Time", value=round(ph7, 2), delta=round((timehotfill-ph7),2)))
+	if product == 'Acid or acidified' and 4.11 <= phhotfill < 4.61 : (st.metric(label = "Treatment Time", value=round(ph78, 2), delta=round((timehotfill-ph8),2)))
+	if phhotfill <= 4.1 and timehotfill > ph7 : st.success('Process time and temperature will mitigate environmental contamintation (vegetative pathogens and spoilage organisms) ')
+	if phhotfill <= 4.1 and timehotfill < ph7 : st.error('Process NOT safe')
+	if  4.11 <= phhotfill < 4.61 and timehotfill > ph8 : st.success('Process time and temperature will mitigate environmental contamintation (vegetative pathogens and spoilage organisms) ')
+	if  4.11 <= phhotfill < 4.61 and timehotfill < ph8 : st.error('Hot Fill Process NOT safe')
+	if product == 'Intermediate foods' : (st.metric(label = "Treatment Time", value=round(IF, 2), delta=round((timehotfill-IF),2)))
+	
         #title = st.markdown('*Title *')
         #G22*(10^((H22-L22)/I22))
         texto = '''Temp = '''
