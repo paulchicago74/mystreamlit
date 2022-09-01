256dccf29f8ae934c5b706dc19f5ee9b0549e9a8
paulchicago74 <paulchicago74@gmail.com>
2022-09-01 16:14:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -22,6 +22,8 @@ import io
 import matplotlib.pyplot as plt
 from streamlit_login_auth_ui.widgets import __login__
 
+__login__obj = __login__(auth_token = "courier_auth_token", company_name = "company_name", width = 200, height = 250, logout_button_name = 'Logout', hide_menu_bool = False, hide_footer_bool = False, lottie_url='https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+LOGGED_IN = __login__obj.build_login_ui()
 
 st.set_page_config(layout="wide")
 
