1b492ceee92416ccc231180c8225d2937aeea2aa
paulchicago74 <paulchicago74@gmail.com>
2022-09-11 12:41:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,51 +2,52 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import io
-import numpy as np
 
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
 # Load the data
 @st.experimental_memo
 def load_data():
     return pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+        {
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+            "Number Eaten": [2, 1, 3, 1, 3, 2],
+        }
+    )
 
 # Create and cache a Plotly figure
 @st.experimental_memo
 def create_figure(df):
-    fig = st.line_chart(chart_data)
+    fig = go.Figure()
+    for contestant, group in df.groupby("Contestant"):
+        fig.add_trace(
+            go.Bar(
+                x=group["Fruit"],
+                y=group["Number Eaten"],
+                name=contestant,
+                hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"
+                % contestant,
+            )
+        )
+    fig.update_layout(legend_title_text="Contestant")
+    fig.update_xaxes(title_text="Fruit")
+    fig.update_yaxes(title_text="Number Eaten")
     return fig
 
-df = load_data(pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c']))
+df = load_data()
 fig = create_figure(df)
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-#st.line_chart(chart_data)
-#fig = create_figure(chart_data)
-
 
 # Create an in-memory buffer
 buffer = io.BytesIO()
 
 # Save the figure as a pdf to the buffer
-#fig.write_image(file=buffer, format="pdf")
-fig.write_image(file=buffer, format="png")
+fig.write_image(file=buffer, format="pdf")
 
 # Download the pdf from the buffer
 st.download_button(
     label="Download PDF",
     data=buffer,
-    file_name="figure.png",
-    mime="application/png",
+    file_name="figure.pdf",
+    mime="application/pdf",
 )
 
-#st.plotly_chart(fig)
+st.plotly_chart(fig)
