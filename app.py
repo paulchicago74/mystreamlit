4a9cab087d68f7d262decf07c25d2cf6c13fa726
paulchicago74 <paulchicago74@gmail.com>
2022-09-11 12:32:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,13 @@ import numpy as np
 @st.experimental_memo
 def load_data():
     return pd.DataFrame(
-        {
-            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-            "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
-            "Number Eaten": [2, 1, 3, 1, 3, 2],
-        }
-    )
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
 # Create and cache a Plotly figure
 @st.experimental_memo
 def create_figure(df):
-    fig = go.Figure()
-    for contestant, group in df.groupby("Contestant"):
-        fig.add_trace(
-            go.Bar(
-                x=group["Fruit"],
-                y=group["Number Eaten"],
-                name=contestant,
-                hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"
-                % contestant,
-            )
-        )
-    fig.update_layout(legend_title_text="Contestant")
-    fig.update_xaxes(title_text="Fruit")
-    fig.update_yaxes(title_text="Number Eaten")
+    fig = st.line_chart(chart_data)
     return fig
 
 df = load_data()
