51c6204c05fa07f8783706d5f19860d177f51ec8
paulchicago74 <paulchicago74@gmail.com>
2022-11-22 10:09:35 -0600

    Update process_capability.py

--- process_capability.py
+++ process_capability.py
@@ -8,3 +8,9 @@ import statsmodels.api as sm
 x = np.arange(10).reshape(-1, 1)
 y = np.array([0, 1, 0, 0, 1, 1, 1, 1, 1, 1])
 x = sm.add_constant(x)
+
+model = sm.Logit(y, x)
+
+result = model.fit(method='newton')
+
+result.params
