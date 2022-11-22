2d0e8b2735293629bf12faa4bcffaf9904abdb40
paulchicago74 <paulchicago74@gmail.com>
2022-11-22 10:17:52 -0600

    Update process_capability.py

--- process_capability.py
+++ process_capability.py
@@ -9,12 +9,13 @@ x = np.arange(10).reshape(-1, 1)
 y = np.array([0, 1, 0, 0, 1, 1, 1, 1, 1, 1])
 x = sm.add_constant(x)
 
-model = sm.Logit(y, x)
+#model = sm.Logit(y, x)
+model = sm.Logit(-44.22)
 
 result = model.fit(method='newton')
 
 result.params
 
-st.write(result.predict(x))
+#st.write(result.predict(x))
 
-st.write(result.summary())
+#st.write(result.summary())
